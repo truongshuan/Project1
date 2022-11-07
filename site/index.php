6fb9604daf5a56fcd3cd048c30d7250ec3959eb2
truongshuan <xuanptpc04031@fpt.edu.vn>
2022-11-07 13:31:17 +0700

    Upload source

---
+++ site/index.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gettree – Garden & Landscaping</title>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link href="https://fonts.googleapis.com/css2?
+    family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Smooch&family=Oswald&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../content/client/css/index.css">
+    <link rel="stylesheet" href="../content/client/css/Grid.css">
+</head>
+
+<body>
+    <!-- infor -->
+    <section class="infor">
+        <div class="infor_left">
+            <div class="infor_left_content"><i class='bx bx-check-circle'></i>
+                ISO 9001:2015,Certified Landscape Designer</div>
+            <div class="infor_left_content"><i class='bx bx-map'></i>
+                55 Main Street, New York</div>
+            <div class="infor_left_content"> <i class='bx bx-time-five'></i>
+                Sun - Friday, 08 am - 05 pm</div>
+        </div>
+        <div class="infor_right">
+            <div>Select Language</div>
+            <div><i class='bx bxl-facebook'></i></div>
+            <div><i class='bx bxl-twitter'></i></div>
+            <div><i class='bx bxl-google'></i></div>
+            <div><i class='bx bxl-instagram-alt'></i></div>
+        </div>
+    </section>
+    <!-- header -->
+    <section class="header">
+        <div class="logo"><img src="../content/client/img/logo.png" alt=""></div>
+        <div class="menu">
+            <div class="menu_ngang">
+                <a href="">Home<i class='bx bx-chevron-down'></i></a>
+                <ul class="menu_con">
+                    <li> <a href=""> Home 1</a></li>
+                    <li> <a href=""> Home 2</a></li>
+                    <li> <a href=""> Home 3</a></li>
+                    <li> <a href=""> Home 4</a></li>
+                </ul>
+            </div>
+            <div class="menu_ngang">
+                <a href="">Page<i class='bx bx-chevron-down'></i></a>
+                <ul class="menu_con">
+                    <li> <a href=""> Home 1</a></li>
+                    <li> <a href=""> Home 2</a></li>
+                    <li> <a href=""> Home 3</a></li>
+                    <li> <a href=""> Home 4</a></li>
+                </ul>
+            </div>
+            <div class="menu_ngang">
+                <a href="">Services<i class='bx bx-chevron-down'></i></a>
+                <ul class="menu_con">
+                    <li> <a href=""> Home 1</a></li>
+                    <li> <a href=""> Home 2</a></li>
+                    <li> <a href=""> Home 3</a></li>
+                    <li> <a href=""> Home 4</a></li>
+                </ul>
+            </div>
+            <div class="menu_ngang">
+                <a href="">Shop<i class='bx bx-chevron-down'></i></a>
+                <ul class="menu_con">
+                    <li> <a href=""> Home 1</a></li>
+                    <li> <a href=""> Home 2</a></li>
+                    <li> <a href=""> Home 3</a></li>
+                    <li> <a href=""> Home 4</a></li>
+                </ul>
+            </div>
+            <div class="menu_ngang">
+                <a href="">Blog<i class='bx bx-chevron-down'></i></a>
+                <ul class="menu_con">
+                    <li> <a href=""> Home 1</a></li>
+                    <li> <a href=""> Home 2</a></li>
+                    <li> <a href=""> Home 3</a></li>
+                    <li> <a href=""> Home 4</a></li>
+                </ul>
+            </div>
+
+            <div class="menu_ngang"> <a href="">Contact</a></div>
+        </div>
+        <div class="header_icon">
+            <div><i class='bx bx-search-alt-2'></i></div>
+            <div><i class='bx bxs-cart'></i></div>
+            <div><i class='bx bx-user-pin'></i></div>
+            <button class="btn_header">Get A Quote <div><i style="font-size: 28px;" class='bx bx-right-arrow-alt'></i></div></button>
+        </div>
+    </section>
+    <!-- content -->
+    <section class="grid wide ">
+        <div class="row garden">
+            <div class="col l-6 garden_trai">
+                <div class="garden1">
+                    <div><i style="color:#348e38;font-size: 30px;" class='bx bx-leaf'></i></div>
+                    <span>Garden Landscaping</span>
+                </div>
+                <p>Build The Green City by Landscaping</p>
+                <span>Gettree garden began operations in 1996 as a residential landscaping lawn maintenance company. From the very beginning, our focus has been attention to detail, professionalism</span>
+                <div class="btn_garden">
+                    <button class="btn_garden1">Get Started<div><i style="font-size: 28px;" class='bx bx-right-arrow-alt'></i></div> </button>
+                    <button class="btn_garden2">How It Works<div><i style="font-size: 28px;" class='bx bx-right-arrow-alt'></i></div></button>
+                </div>
+            </div>
+            <div class="col l-6"><img src="../content/client/img/about1.jfif" alt=""></div>
+        </div>
+    </section>
+    <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#ffffff" fill-opacity="1" d="M0,288L24,282.7C48,277,96,267,144,250.7C192,235,240,213,288,208C336,203,384,213,432,229.3C480,245,528,267,576,240C624,213,672,139,720,128C768,117,816,171,864,176C912,181,960,139,1008,144C1056,149,1104,203,1152,218.7C1200,235,1248,213,1296,218.7C1344,224,1392,256,1416,272L1440,288L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z"></path>
+    </svg>
+    <!-- offer -->
+    <section class="offer" style="background-image:linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(../content/client/img/bg_offer.jfif);">
+        <p>What We Offer</p>
+        <h1>Make Your Dream Landscaping <br>
+            Garden With Gettree</h1>
+        <section class="grid wide">
+            <div class="row">
+                <div class="col l-3">
+                    <div class="offer1">
+                        <div class="offer1_circle"><i class='bx bx-color'></i></div>
+                        <p>Landscaping</p>
+                        <span>Quis autem vel eum iure ende scnse quats</span>
+                        <div class="read_more"><a href="">Read More</a></div>     
+                    </div>
+                </div>
+                <div class="col l-3">
+                <div class="offer1">
+                        <div class="offer1_circle"><i class='bx bx-color'></i></div>
+                        <p>Landscaping</p>
+                        <span>Quis autem vel eum iure ende scnse quats</span>
+                        <div class="read_more"><a href="">Read More</a></div>     
+                    </div>
+                </div>
+                <div class="col l-3">
+                <div class="offer1">
+                        <div class="offer1_circle"><i class='bx bx-color'></i></div>
+                        <p>Landscaping</p>
+                        <span>Quis autem vel eum iure ende scnse quats</span>
+                        <div class="read_more"><a href="">Read More</a></div>     
+                    </div>
+                </div>
+                <div class="col l-3">
+                <div class="offer1">
+                        <div class="offer1_circle"><i class='bx bx-color'></i></div>
+                        <p>Landscaping</p>
+                        <span>Quis autem vel eum iure ende scnse quats</span>
+                        <div class="read_more"><a href="">Read More</a></div>     
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+    <!-- about -->
+    <section class="grid wide">
+        <div class="row about">
+            <div class="col l-6"><img src="../content/client/img/blob1.jfif" alt=""></div>
+            <div class="col l-6 about_phai">
+                <div class="about1">
+                    <div><i style="color:#348e38;font-size: 30px;" class='bx bx-leaf'></i></div>
+                    <span>About Company</span>
+                </div>
+                <p>Professional Service Company For Landscaping</p>
+                <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</span>
+                <div class="about2">
+                    <div class="about2_icon"><i class='bx bxs-tree-alt'></i></div>
+                    <div class="about2_content">
+                        <p>Professional Landscaping</p>
+                        <span>Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium dolore</span>
+                    </div>
+                </div>
+                <div class="about2">
+                    <div class="about2_icon"><i class='bx bxs-tree-alt'></i></div>
+                    <div class="about2_content">
+                        <p>Professional Landscaping</p>
+                        <span>Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium dolore</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Our Latest Service -->
+    <section class="service">
+        <p>Our Latest Service</p>
+        <h1>We Provide Awesome Service
+            <br>
+            For Landscaping
+        </h1>
+        <div class="row">
+            <div class="col"></div>
+        </div>
+        <div class="services">
+            <div class="services_box">
+                <div class="services_box_top">
+                    <i class='bx bxs-tree-alt'></i>
+                    <p>Landscaping</p>
+                    <span>Quis autem vel eum iure ende scnse quats</span>
+                </div>
+                <div class="services_box_bot">
+                    <img src="../content/client/img/service1.jfif" alt="">
+                </div>
+            </div>
+            <div class="services_box">
+                <div class="services_box_top">
+                    <i class='bx bxs-tree-alt'></i>
+                    <p>Pruning Plants</p>
+                    <span>Quis autem vel eum iure ende scnse quats</span>
+                </div>
+                <div class="services_box_bot">
+                    <img src="../content/client/img/service2.jfif" alt="">
+                </div>
+            </div>
+            <div class="services_box">
+                <div class="services_box_top">
+                    <i class='bx bxs-tree-alt'></i>
+                    <p>Lawn Maintenance</p>
+                    <span>Quis autem vel eum iure ende scnse quats</span>
+                </div>
+                <div class="services_box_bot">
+                    <img src="../content/client/img/service3.jfif" alt="">
+                </div>
+            </div>
+            <div class="services_box">
+                <div class="services_box_top">
+                    <i class='bx bxs-tree-alt'></i>
+                    <p>Lawn Moving</p>
+                    <span>Quis autem vel eum iure ende scnse quats</span>
+                </div>
+                <div class="services_box_bot">
+                    <img src="../content/client/img/service4.jfif" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- recore -->
+    <section class="recore" style="background-image:linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url(../content/client/img/record.jfif);">
+        <section class="grid wide">
+            <div class="row">
+                <div class="col l-3">
+                    <div class="recore1">
+                        <div class="recore1_top"><i class='bx bx-leaf'></i>
+                            <p>325+</p>
+                        </div>
+                        <p>Completed Projects</p>
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="recore1">
+                        <div class="recore1_top"><i class='bx bx-leaf'></i>
+                            <p>785+</p>
+                        </div>
+                        <p>Expert Landscapers</p>
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="recore1">
+                        <div class="recore1_top"><i class='bx bx-leaf'></i>
+                            <p>490+</p>
+                        </div>
+                        <p>Satisfied Customers</p>
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="recore1">
+                        <div class="recore1_top"><i class='bx bx-leaf'></i>
+                            <p>360+</p>
+                        </div>
+                        <p>Awards And Honor</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+    <p class="p">What We Offer</p>
+    <h1>We Have 3+ Experience
+        <br>
+        Team Members
+    </h1>
+    <!-- team -->
+    <section class="grid wide team">
+        <div class="row team">
+
+        </div>
+    </section>
+</body>
+
+</html+
