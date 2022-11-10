4906c23aac4dd6921e1023c022c5e44334b694b5
truongshuan <xuanptpc04031@fpt.edu.vn>
2022-11-10 19:47:02 +0700

    Upload source first

--- site/index.php
+++ site/index.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="../content/client/css/index.css">
     <link rel="stylesheet" href="../content/client/css/Grid.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
 </head>
 
 <body>
@@ -18,7 +20,7 @@
             <div class="infor_left_content"><i class='bx bx-check-circle'></i>
                 ISO 9001:2015,Certified Landscape Designer</div>
             <div class="infor_left_content"><i class='bx bx-map'></i>
-                55 Main Street, New York</div>
+                331 An Khánh, Cần Thơ</div>
             <div class="infor_left_content"> <i class='bx bx-time-five'></i>
                 Sun - Friday, 08 am - 05 pm</div>
         </div>
@@ -69,7 +71,7 @@
     <!-- content -->
     <section class="grid wide">
         <div class="row garden">
-            <div class="col l-6 garden_trai">
+            <div class="col l-6 garden_trai " data-aos="fade-right" data-aos-duration="1000">
                 <div class="garden1">
                     <div><i style="color:#348e38;font-size: 30px;" class='bx bx-leaf'></i></div>
                     <span>Garden Landscaping</span>
@@ -78,23 +80,28 @@
                 <span>Gettree garden began operations in 1996 as a residential landscaping lawn maintenance company.
                     From the very beginning, our focus has been attention to detail, professionalism</span>
                 <div class="btn_garden">
-                    <button class="btn_garden1">Get Started<div><i style="font-size: 28px;" class='bx bx-right-arrow-alt'></i></div> </button>
-                    <button class="btn_garden2">How It Works<div><i style="font-size: 28px;" class='bx bx-right-arrow-alt'></i></div></button>
+                    <button class="btn_garden1">Get Started<div><i style="font-size: 28px;"
+                                class='bx bx-right-arrow-alt'></i></div> </button>
+                    <button class="btn_garden2">How It Works<div><i style="font-size: 28px;"
+                                class='bx bx-right-arrow-alt'></i></div></button>
                 </div>
             </div>
-            <div class="col l-6"><img src="../content/client/img/about1.jfif" alt=""></div>
+            <div class="col l-6" data-aos="fade-left" data-aos-duration="1000"><img
+                    src="../content/client/img/about1.jfif" alt=""></div>
         </div>
     </section>
     <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path fill="#ffffff" fill-opacity="1" d="M0,288L24,282.7C48,277,96,267,144,250.7C192,235,240,213,288,208C336,203,384,213,432,229.3C480,245,528,267,576,240C624,213,672,139,720,128C768,117,816,171,864,176C912,181,960,139,1008,144C1056,149,1104,203,1152,218.7C1200,235,1248,213,1296,218.7C1344,224,1392,256,1416,272L1440,288L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z">
+        <path fill="#ffffff" fill-opacity="1"
+            d="M0,288L24,282.7C48,277,96,267,144,250.7C192,235,240,213,288,208C336,203,384,213,432,229.3C480,245,528,267,576,240C624,213,672,139,720,128C768,117,816,171,864,176C912,181,960,139,1008,144C1056,149,1104,203,1152,218.7C1200,235,1248,213,1296,218.7C1344,224,1392,256,1416,272L1440,288L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z">
         </path>
     </svg>
     <!-- offer -->
-    <section class="offer" style="background-image:linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(../content/client/img/bg_offer.jfif);">
-        <p>What We Offer</p>
-        <h1>Make Your Dream Landscaping <br>
+    <section class="offer"
+        style="background-image:linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(../content/client/img/bg_offer.jfif);">
+        <p data-aos="fade-up " data-aos-duration="1000">What We Offer</p>
+        <h1 data-aos="fade-up" data-aos-duration="1000">Make Your Dream Landscaping <br>
             Garden With Gettree</h1>
-        <section class="grid wide">
+        <section class="grid wide" data-aos="fade-up" data-aos-duration="1000">
             <div class="row">
                 <div class="col l-3">
                     <div class="offer1">
@@ -134,10 +141,10 @@
     <!-- about -->
     <section class="grid wide">
         <div class="row about">
-            <div class="col l-6 about_img">
+            <div class="col l-6 about_img" data-aos="fade-right" data-aos-duration="1000">
                 <img src="../content/client/img/z.png" alt="">
             </div>
-            <div class="col l-6 about_phai">
+            <div class="col l-6 about_phai" data-aos="fade-left" data-aos-duration="1000">
                 <div class="about1">
                     <div><i style="color:#348e38;font-size: 30px;" class='bx bx-leaf'></i></div>
                     <span>About Company</span>
@@ -215,10 +222,11 @@
         </div>
     </section>
     <!-- recore -->
-    <section class="recore" style="background-image:linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url(../content/client/img/record.jfif);">
+    <section class="recore"
+        style="background-image:linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url(../content/client/img/record.jfif);">
         <section class="grid wide">
             <div class="row">
-                <div class="col l-3">
+                <div class="col l-3" data-aos="zoom-in" data-aos-duration="500">
                     <div class="recore1">
                         <div class="recore1_top"><i class='bx bx-leaf'></i>
                             <p>325+</p>
@@ -226,7 +234,7 @@
                         <p>Completed Projects</p>
                     </div>
                 </div>
-                <div class="col l-3">
+                <div class="col l-3" data-aos="zoom-in" data-aos-duration="500">
                     <div class="recore1">
                         <div class="recore1_top"><i class='bx bx-leaf'></i>
                             <p>785+</p>
@@ -234,7 +242,7 @@
                         <p>Expert Landscapers</p>
                     </div>
                 </div>
-                <div class="col l-3">
+                <div class="col l-3" data-aos="zoom-in" data-aos-duration="500">
                     <div class="recore1">
                         <div class="recore1_top"><i class='bx bx-leaf'></i>
                             <p>490+</p>
@@ -242,7 +250,7 @@
                         <p>Satisfied Customers</p>
                     </div>
                 </div>
-                <div class="col l-3">
+                <div class="col l-3" data-aos="zoom-in" data-aos-duration="500">
                     <div class="recore1">
                         <div class="recore1_top"><i class='bx bx-leaf'></i>
                             <p>360+</p>
@@ -333,25 +341,29 @@
     </section>
     <!-- hinh anh garden -->
     <section class="img_garden">
-        <div class="img_garden1" style="background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.4)), url(../content/client/img/img_garden1.jpg);">
+        <div class="img_garden1" data-aos="fade-up" data-aos-duration="500"
+            style="background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.4)), url(../content/client/img/img_garden1.jpg);">
             <div class="img_garden1_content">
                 <p>Gardening Watering</p>
                 <b>Landscaping</b>
             </div>
         </div>
-        <div class="img_garden1" style="background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.4)), url(../content/client/img/img_garden2.jpg);">
+        <div class="img_garden1" data-aos="fade-up" data-aos-duration="500"
+            style="background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.4)), url(../content/client/img/img_garden2.jpg);">
             <div class="img_garden1_content">
                 <p>Gardening Watering</p>
                 <b>Landscaping</b>
             </div>
         </div>
-        <div class="img_garden1" style="background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.4)), url(../content/client/img/img_garden3.jpg);">
+        <div class="img_garden1" data-aos="fade-up" data-aos-duration="500"
+            style="background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.4)), url(../content/client/img/img_garden3.jpg);">
             <div class="img_garden1_content">
                 <p>Gardening Watering</p>
                 <b>Landscaping</b>
             </div>
         </div>
-        <div class="img_garden1" style="background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.4)), url(../content/client/img/img_garden4.jpg);">
+        <div class="img_garden1" data-aos="fade-up" data-aos-duration="500"
+            style="background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.4)), url(../content/client/img/img_garden4.jpg);">
             <div class="img_garden1_content">
                 <p>Gardening Watering</p>
                 <b>Landscaping</b>
@@ -362,17 +374,19 @@
     <section class="bg_feedback">
         <section class="grid wide">
             <div class="row">
-                <div class="col l-12">
+                <div class="col l-12" data-aos="fade-up" data-aos-duration="1000">
                     <div class="feedback">
                         <div class="feedback_img">
-                            <img src="../content/client/img/feedback.jpg.crdownload" alt="">
+                            <img src="../content/client/img/feedback1.jpg" alt="">
                         </div>
                         <div class="feedback_content">
                             <b>Client Feedback</b>
                             <p>What’s Our Clients <br>
                                 Say About Us</p>
                             <div class="feedback_box">
-                                <p>Sed ut perspiciatis unde omnis iste natus voltem accusantium doloremque laudantium totam reaperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                                <p>Sed ut perspiciatis unde omnis iste natus voltem accusantium doloremque laudantium
+                                    totam reaperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto
+                                    beatae vitae dicta sunt explicabo.</p>
                                 <div class="client_feedback">
                                     <img src="../content/client/img/avatar.png" alt="">
                                     <div>
@@ -389,6 +403,7 @@
         </section>
 
     </section>
+    <!-- new blog -->
     <section class="new_blog">
         <p>Latest News & Blog</p>
         <h1>Get Every Single Update
@@ -398,7 +413,7 @@
         </h1>
         <div class="grid wide ">
             <div class="row">
-                <div class="col l-4">
+                <div class="col l-4" data-aos="fade-up" data-aos-delay="100" data-aos-duration="800">
                     <div class="box_new">
                         <div class="box_new_top">
                             <img src="../content/client/img/new1.jpg" alt="">
@@ -414,7 +429,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col l-4"> <div class="box_new">
+                <div class="col l-4" data-aos="fade-up" data-aos-delay="200" data-aos-duration="800">
+                    <div class="box_new">
                         <div class="box_new_top">
                             <img src="../content/client/img/new2.jpg" alt="">
                         </div>
@@ -427,8 +443,10 @@
                             <hr>
                             <div><i class='bx bx-message-rounded-dots'></i> COMMENT (0)</div>
                         </div>
-                    </div></div>
-                <div class="col l-4"> <div class="box_new">
+                    </div>
+                </div>
+                <div class="col l-4" data-aos="fade-up" data-aos-delay="300" data-aos-duration="800">
+                    <div class="box_new">
                         <div class="box_new_top">
                             <img src="../content/client/img/new3.jpg" alt="">
                         </div>
@@ -437,15 +455,131 @@
                             <b>The top 5 garden renovation trends this</b>
                         </div>
                         <div class="box_new_bot">
-                            <div><i class='bx bx-notepad'></i> 	Februar 25, 2022</div>
+                            <div><i class='bx bx-notepad'></i> Februar 25, 2022</div>
                             <hr>
                             <div><i class='bx bx-message-rounded-dots'></i> COMMENT (0)</div>
                         </div>
-                    </div></div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-    <h1>adasdas</h1>
+    <section class="foodter_img">
+        <img src="../content/client/img/foodter.jpg" alt="">
+    </section>
+    <footer>
+        <section class="grid wide contact_home">
+            <div class="row">
+                <div class="col l-3">
+                    <div class="contact_home1">
+                        <img src="../content/client/img/logo.png" alt="">
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="contact_home2">
+                        <i class='bx bxs-map'></i>
+                        <div class="contact_home2_content">
+                            <p> Address</p>
+                            <b>Cần Thơ</b>
+                        </div>
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="contact_home2">
+                        <i class='bx bx-phone'></i>
+                        <div class="contact_home2_content">
+                            <p> Hotline</p>
+                            <b>0941477074</b>
+                        </div>
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="contact_home2">
+                        <i class='bx bx-envelope'></i>
+                        <div class="contact_home2_content">
+                            <p> Email Us</p>
+                            <b>hoangthuan@gmail.com</b>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="grid wide about_home">
+            <div class="row">
+                <div class="col l-3">
+                    <div class="about_home1">
+                        <p>About Us</p>
+
+                        <b>Consectetur adipiscing elit sed tempor incididunt ut labore dolore magna aliqua. Quis suspe
+                            ndisse ultrices gravida risus</b>
+                        <div class="icon_about_home1">
+                            <i class='bx bx-envelope'></i>
+                            <i class='bx bxl-facebook'></i>
+                            <i class='bx bxl-twitter'></i>
+                            <i class='bx bxl-instagram-alt'></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="about_home2">
+                        <p>Explore</p>
+                        <ul class="ul_about_home2">
+                            <li>Company Story</li>
+                            <li>Latest Services</li>
+                            <li>Meet The Team</li>
+                            <li>Need a Career?</li>
+                            <li>Setting & Privacy</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="about_home3">
+                        <p>Recent Post</p>
+                        <div class="about_home3_1">
+                            <img src="../content/client/img/foodter1.jpg" alt="">
+                            <div>
+                                <p>16 garden design ideas to make</p>
+                                <div class="about_home3_1_icon">
+                                    <i class='bx bx-envelope'></i> May 8, 2021
+                                </div>
+                            </div>
+                        </div>
+                        <div class="about_home3_1">
+                            <img src="../content/client/img/foodter2.jpg" alt="">
+                            <div>
+                                <p>
+                                    How to grow and arrange your</p>
+                                <div class="about_home3_1_icon">
+                                    <i class='bx bx-envelope'></i> May 8, 2021
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="about_home4">
+                        <p>Newsletter</p>
+                        <b>Tempor incididunt ut labore dolore magna aliqua suspe ndisse</b>
+                        <br>
+                        <div class="input_foodter_box">
+                            <input type="text" class="input_foodter" placeholder="Email Address">
+                            <div class="input_foodter_email">
+                                <i class='bx bx-envelope'></i>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <p>Copyright &copy 2021 The Gettree Landscaping. Designed by H. Thuận, M. Tý and T. Xuân</p>
+        </section>
+    </footer>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+    AOS.init();
+    </script>
 </body>
 
 </html>
