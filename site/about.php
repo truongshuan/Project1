6c5f125b4fba87e0136e0995205365e647e235b9
thuan <hoangthuan02022000@gmail.com>
2022-11-12 17:56:12 +0700

    upload about

--- site/about.php
+++ site/about.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,20 +11,21 @@
     <link rel="stylesheet" href="../content/client/css/Grid.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
+
 <body>
     <!-- header -->
-    <?php 
-        include "page/header.php";
+    <?php
+    include "page/header.php";
     ?>
     <section class="tiltle_about" style="background-image:linear-gradient(rgba(15,66,41,0.95),rgba(15,66,41,0.95)),url(../content/client/img/record.jfif);">
-    <div class="tiltle_about_content">
-        <p>About Us</p>
-        <div class="tiltle_about_content1">
-            <a href="">Home</a>
-            <hr>
-            <a href="">About Us</a>
+        <div class="tiltle_about_content">
+            <p>About Us</p>
+            <div class="tiltle_about_content1">
+                <a href="">Home</a>
+                <hr>
+                <a href="">About Us</a>
+            </div>
         </div>
-    </div>
     </section>
     <!-- about us -->
     <section class="grid wide about_us">
@@ -42,9 +44,173 @@
                 </div>
             </div>
             <div class="col l-6">
+                <div class="about_us_right">
+                    <span>ABOUT US</span>
+                    <p>We are available for
+                        24/7 for Garden Services</p>
+                    <b>We began with a single vision shared between 10 brothers. Originally, we opened our doors as Care in 1987. As time passed and more brothers joined the project. <b>We provide main service</b></b>
+                    <div class="box">
+                        <div class="left">
+                            <div class="content">
+                                <div class="icon">
+                                    <i class='bx bx-building-house'></i>
+                                </div>
+                                <div class="tiltle">
+                                    <p>Residential Services</p>
+                                    <b>The Gettree elite team of</b>
 
+                                </div>
+                            </div>
+                            <div class="content">
+                                <div class="icon">
+                                    <i class='bx bxs-group'></i>
+                                </div>
+                                <div class="tiltle">
+                                    <p>Commercial Services</p>
+                                    <b>The Gettree elite team of</b>
+
+                                </div>
+                            </div>
+                            <button class="button">
+                                Get A Quoet
+                            </button>
+                        </div>
+                        <div class="right">
+                            <img src="../content/client/img/aboutus1.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
+    <!-- work -->
+    <section class="grid wide work ">
+        <p>WORK PROCESS</p>
+        <h1>We Complete Every
+            <br>
+            Step Carefully
+        </h1>
+        <div class="row works">
+            <div class="col l-6">
+                <div class="work1">
+                    <div class="left" count="1"><i class='bx bx-shower'></i></div>
+                    <div class="right">
+                        <p>Consulting & Estimation</p>
+                        <b>Lorem Ipsum is simply dummy text of free available market typesetting industry has been the</b>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col l-6">
+                <div class="work1">
+                    <div class="left" count="2"><i class='bx bx-shield-plus'></i></div>
+                    <div class="right">
+                        <p>Design & Choose Material</p>
+                        <b>Lorem Ipsum is simply dummy text of free available market typesetting industry has been the</b>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col l-6">
+                <div class="work1">
+                    <div class="left" count="3"><i class='bx bx-podcast'></i></div>
+                    <div class="right">
+                        <p>Proceed To Work</p>
+                        <b>Lorem Ipsum is simply dummy text of free available market typesetting industry has been the</b>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col l-6">
+                <div class="work1">
+                    <div class="left" count="4"><i class='bx bx-ghost'></i></div>
+                    <div class="right">
+                        <p>Complete & Maintenance</p>
+                        <b>Lorem Ipsum is simply dummy text of free available market typesetting industry has been the</b>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- recore -->
+    <section class="recore_about">
+        <div class="grid wide">
+            <div class="row">
+                <div class="col l-9">
+                    <div class="img_recore">
+                        <img src="../content/client/img/bg_about.png" alt="">
+                        <div class="content_recore">
+
+                            <p>We are nice people with a lot of experience.</p>
+                            <b><b>Our Mission:</b> is to provide our customers with responsive service and unsurpassed quality a very competitive price</b>
+                        </div>
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <div class="recore1">
+                        <p>10+</p>
+                        <b>Years of experience</b>
+
+                    </div>
+                    <div class="recore1">
+                        <p>95K</p>
+                        <b>Happy Customers</b>
+                    </div>
+                    <div class="recore1">
+                        <p>100%</p>
+                        <b>Satisfaction</b>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- feedback -->
+    <section class="grid wide">
+        <div class="row feedback">
+            <div class="col l-5">
+                <div class="left">
+                    <span>CLIENT TESTIMONIALS</span>
+                    <p>What our clients say</p>
+                    <b>We pride ourselves on making sure every client is 100% satisfied. Here’s what a few of our clients say about us…</b>
+                    <div class="box">
+                        <div class="box1">
+                            <div class="top">
+                                <img src="../content/client/img/feedback2.jpg" alt="">
+                                <div class="content">
+                                    <p>Nguyễn Thúy Quyên</p>
+                                    <b>Client</b>
+                                </div>
+                            </div>
+                            <p>All I can say is wow! Very professional and good at what they do! I highly recommend it!</p>
+                        </div>
+                        <div class="box1">
+                        <div class="top">
+                                <img src="../content/client/img/feedback3.jpg" alt="">
+                                <div class="content">
+                                    <p>Võ Hoàng Thuận</p>
+                                    <b>Client</b>
+                                </div>
+                            </div>
+                            <p>Gettree Landscaping Services has done such a great job on all of our landscaping, I rate 5 stars</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col l-7">
+                <div class="right">
+                    <img src="../content/client/img/feedback1.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="foodter_img">
+        <img src="../content/client/img/foodter.jpg" alt="">
+    </section>
+    <!-- foodter -->
+    <?php 
+        include "page/foodter.php"
+    ?>
 </body>
+
 </html>
