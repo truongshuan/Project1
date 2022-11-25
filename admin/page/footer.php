0f859d8f7c3e10e74025f256bf2ab80586ba24b1
truongshuan <xuanptpc04031@fpt.edu.vn>
2022-11-25 09:15:19 +0700

    Upload src Admin

---
+++ admin/page/footer.php
@@ -0,0 +1,47 @@
+    <!-- latest jquery-->
+    <script src="../content/admin/js/jquery-3.5.1.min.js"></script>
+    <!-- feather icon js-->
+    <script src="../content/admin/js/icons/feather-icon/feather.min.js"></script>
+    <script src="../content/admin/js/icons/feather-icon/feather-icon.js"></script>
+    <!-- Sidebar jquery-->
+    <script src="../content/admin/js/sidebar-menu.js"></script>
+    <script src="../content/admin/js/config.js"></script>
+    <!-- Bootstrap js-->
+    <script src="../content/admin/js/bootstrap/popper.min.js"></script>
+    <script src="../content/admin/js/bootstrap/bootstrap.min.js"></script>
+    <!-- Plugins JS start-->
+    <script src="../content/admin/js/chart/chartist/chartist.js"></script>
+    <script src="../content/admin/js/chart/chartist/chartist-plugin-tooltip.js"></script>
+    <script src="../content/admin/js/chart/knob/knob.min.js"></script>
+    <script src="../content/admin/js/chart/knob/knob-chart.js"></script>
+    <script src="../content/admin/js/chart/apex-chart/apex-chart.js"></script>
+    <script src="../content/admin/js/chart/apex-chart/stock-prices.js"></script>
+    <script src="../content/admin/js/prism/prism.min.js"></script>
+    <script src="../content/admin/js/clipboard/clipboard.min.js"></script>
+    <script src="../content/admin/js/counter/jquery.waypoints.min.js"></script>
+    <script src="../content/admin/js/counter/jquery.counterup.min.js"></script>
+    <script src="../content/admin/js/counter/counter-custom.js"></script>
+    <script src="../content/admin/js/custom-card/custom-card.js"></script>
+    <script src="../content/admin/js/notify/bootstrap-notify.min.js"></script>
+    <script src="../content/admin/js/vector-map/jquery-jvectormap-2.0.2.min.js"></script>
+    <script src="../content/admin/js/vector-map/map/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="../content/admin/js/vector-map/map/jquery-jvectormap-us-aea-en.js"></script>
+    <script src="../content/admin/js/vector-map/map/jquery-jvectormap-uk-mill-en.js"></script>
+    <script src="../content/admin/js/vector-map/map/jquery-jvectormap-au-mill.js"></script>
+    <script src="../content/admin/js/vector-map/map/jquery-jvectormap-chicago-mill-en.js"></script>
+    <script src="../content/admin/js/vector-map/map/jquery-jvectormap-in-mill.js"></script>
+    <script src="../content/admin/js/vector-map/map/jquery-jvectormap-asia-mill.js"></script>
+    <script src="../content/admin/js/dashboard/default.js"></script>
+    <script src="../content/admin/js/notify/index.js"></script>
+    <script src="../content/admin/js/datepicker/date-picker/datepicker.js"></script>
+    <script src="../content/admin/js/datepicker/date-picker/datepicker.en.js"></script>
+    <script src="../content/admin/js/datepicker/date-picker/datepicker.custom.js"></script>
+    <!-- Plugins JS Ends-->
+    <!-- Theme js-->
+    <script src="../content/admin/js/script.js"></script>
+    <script src="../content/admin/js/theme-customizer/customizer.js"></script>
+    <!-- login js-->
+    <!-- Plugin used-->
+    </body>
+
+    </html>
---
+++ admin/page/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="viho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords"
+        content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+    <link rel="icon" href="../content/admin/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../content/admin/images/favicon.png" type="image/x-icon">
+    <title>Gettree - Admin</title>
+    <!-- Google font-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="../../css2.css?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+    <link
+        href="../../css2-1.css?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+        rel="stylesheet">
+    <link
+        href="../../css2-2.css?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+    <!-- Font Awesome-->
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/fontawesome.css">
+    <script src="https://kit.fontawesome.com/088d757bb6.js" crossorigin="anonymous"></script>
+    <!-- ico-font-->
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/icofont.css">
+    <!-- Themify icon-->
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/themify.css">
+    <!-- Flag icon-->
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/flag-icon.css">
+    <!-- Feather icon-->
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/feather-icon.css">
+    <!-- Plugins css start-->
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/chartist.css">
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/date-picker.css">
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/prism.css">
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/vector-map.css">
+    <!-- Plugins css Ends-->
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/bootstrap.css">
+    <!-- App css-->
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/style.css">
+    <link id="color" rel="stylesheet" href="../content/admin/css/color-1.css" media="screen">
+    <!-- Responsive css-->
+    <link rel="stylesheet" type="text/css" href="../content/admin/css/responsive.css">
+</head>
